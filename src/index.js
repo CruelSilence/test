8ba45d67e5b98f57ad8520357a80f8fb99ad89fa
lmv <lumaxus@gmail.com>
2021-05-04 16:29:07 +0300

    some commit FEAT2

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const queryClient = new QueryClient();
 
 function App() {
   const [postId, setPostId] = React.useState(-1);
-  console.log('10');
+  console.log('2000');
 
   useEffect(() => {
     test('wtf_master ololo');
